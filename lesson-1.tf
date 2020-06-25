7b72c027eea1c5c4c5261cc48e058122a488e128
valikkr <valikkr@ukr.net>
2020-06-25 03:51:22 -0700

    web terraform

--- lesson-1.tf
+++ lesson-1.tf
@@ -4,15 +4,49 @@ provider "aws" {
     secret_key = var.secret_id
 }
 
-
-resource "aws_instance" "my_ubuntu" {
-    count = 1
-    ami = "ami-089cc16f7f08c4457"
-    instance_type = "t2.micro"
+resource "aws_instance" "my_web_server" {
+      ami = "ami-0ea3405d2d2522162"
+        instance_type = "t2.micro"
+        vpc_security_group_ids = [aws_security_group.my_web_server.id]
+        user_data = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!" > /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+    tags = {
+        Name = "Web Server Build by terraform"
+        Owner = "Valentine Kravtsov"
+    }
 }
 
-resource "aws_instance" "my_linux" {
-    count = 1
-    ami = "ami-0ea3405d2d2522162"
-    instance_type = "t2.micro"
+resource "aws_security_group" "my_web_server" {
+    name = "my_web_server"
+    description = "My_First_sec_group"
+  
+
+
+    ingress {
+        from_port = 80
+        protocol = "tcp"
+        to_port = 80
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        protocol = "tcp"
+        to_port = 443
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    
+    egress {
+        from_port = 0
+        protocol = "-1"
+        to_port = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
